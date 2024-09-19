88d424f4d5695f15b1ec5e505e282c947ffca9f8
Mohamed Mahmoud <mohamedmkamal19@gmail.com>
2024-09-19 18:48:25 +0300

    remove

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import  ProductPage  from './component/ProductPage';
 import OrderForm from './component/OrderForm/OrderForm';
 import NavBar from './component/Navbar';
-import OrdersTable from './page/OrdersTable';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<ProductPage />} />
         <Route path="/order" element={<OrderForm />} />
-        <Route path="/OrdersTable" element={<OrdersTable />} />
       </Routes>
     </Router>
   );
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,16 +5,11 @@ const NavBar = () => {
     <nav className="bg-gray-800 p-4">
       <div className="max-w-7xl mx-auto flex justify-between">
         <h1 className="text-white text-2xl font-bold">Dev/Mohamed</h1>
-        <ul className="flex space-x-4">
+        <ul className="flex space-x-4 gap-5">
           <li>
             <Link to="/" className="text-white hover:text-gray-300">Product</Link>
           </li>
-          <li>
-            <Link to="/OrdersTable" className="text-white hover:text-gray-300">tableData</Link>
-          </li>
-          <li>
-            {/* <Link to="/order" className="text-white hover:text-gray-300">Order</Link> */}
-          </li>
+
         </ul>
       </div>
     </nav>
